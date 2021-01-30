1ece34ccb01888e5cdb479285113add8559df229
t-taiki0620 <38490389+t-taiki0620@users.noreply.github.com>
2021-01-30 16:23:20 +0900

    second time

--- scripts/main.js
+++ scripts/main.js
@@ -3,20 +3,18 @@ alert('hello');
 myVariable = 'Bob';
 
 let myImage = document.querySelector('img');
-
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
         myImage.setAttribute ('src', 'images/firefox2.png');
     }
     else{
-        myImage.setAttribute ('scr', 'images/firfox-icon.png')
+        myImage.setAttribute ('src', 'images/firefox-icon.png')
     }
 }
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
 function setUserName() {
     let myName = prompt('Please enter your name.');
     if(!myName || myName === null) {
@@ -26,14 +24,12 @@ function setUserName() {
         myHeading.innerHTML = 'Mozilla is cool, ' + myName;
     }
 }
-
 if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = 'Mozilla is cool, ' + storedName;
 }
-
 myButton.onclick = function() {
     setUserName();
 }
